76d4b59b49e5dd6ab6ee84887d0631ea354a1ff4
H4ntz-M4d <muhadiahmad71@gmail.com>
2024-06-10 14:05:51 +0700

    fix design and bug detail_absen

--- lib/user/model/event_list.dart
+++ lib/user/model/event_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:project_attendance_app/user/model/record_absen.dart';
 class EventsList extends StatelessWidget {
@@ -25,17 +26,30 @@ class EventsList extends StatelessWidget {
         itemCount: events.length,
         itemBuilder: (context, index) {
           Color eventColor;
+          var sizedBoxAttribute;
+          var sizedBoxStatus;
 
           // Determine the color based on the event status
           switch (events[index].namaKeterangan.toUpperCase()) {
             case 'ALPHA':
-              eventColor = Colors.red;
+              eventColor = const Color(0xffF468B7);
+              sizedBoxAttribute = 20.0;
+              sizedBoxStatus = 6.0;
               break;
             case 'HADIR':
-              eventColor = Colors.green;
+              eventColor = const Color(0xff5FD0D3);
+              sizedBoxAttribute = 20.0;
+              sizedBoxStatus = 0.0;
               break;
             case 'IZIN':
-              eventColor = Colors.yellow;
+              eventColor = const Color(0xffFEC85C);
+              sizedBoxAttribute = 20.0;
+              sizedBoxStatus = 6.0;
+              break;
+            case 'SAKIT':
+              eventColor = const Color(0xff8D7AEE);
+              sizedBoxAttribute = 20.0;
+              sizedBoxStatus = 0.0;
               break;
             default:
               eventColor = Colors.grey;
@@ -44,17 +58,70 @@ class EventsList extends StatelessWidget {
           final timeFormat = DateFormat('HH:mm');
           final formattedTime = timeFormat.format(events[index].record);
           return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
             decoration: BoxDecoration(
-              border: Border.all(),
+              border:
+                  Border.all(color: eventColor), // Use event color for border
               borderRadius: BorderRadius.circular(12),
+              color: eventColor
+                  .withOpacity(0.1), // Light background color based on status
             ),
             child: ListTile(
-              title: Text(
-                events[index].namaKeterangan,
-                style: TextStyle(color: eventColor),
+              title: Row(
+                children: [
+                  SizedBox(
+                    width: sizedBoxStatus,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      events[index].namaKeterangan,
+                      style: GoogleFonts.plusJakartaSans(
+                            textStyle: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              color: eventColor,
+                            ),
+                          ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: sizedBoxStatus,
+                  ),
+                  Container(
+                    width: 5,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: eventColor,
+                      borderRadius: BorderRadius.circular(3),
+                    ),
+                  ),
+                  SizedBox(
+                    width: sizedBoxAttribute,
+                  ),
+                  Text(
+                    formattedTime,
+                    style: TextStyle(color: eventColor),
+                  ),
+                  SizedBox(
+                    width: sizedBoxAttribute,
+                  ),
+                  Center(
+                    child: Row (
+                      children: [
+                        Text(
+                          events[index].nis,
+                          style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              color: eventColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              subtitle: Text(formattedTime),
             ),
           );
         },
