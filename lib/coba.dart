6661c6e10dea723e3338686f1899fe364525c639
WildaniDanialNafis <wildaninafis203@gmail.com>
2024-06-21 19:32:55 +0700

    coba fix detail_absen

--- lib/coba.dart
+++ lib/coba.dart
@@ -7,6 +7,7 @@ import 'package:project_attendance_app/user/authentication/login_layout.dart';
 import 'package:project_attendance_app/user/fragments/about_screen.dart';
 import 'package:project_attendance_app/user/fragments/account_screen.dart';
 import 'package:project_attendance_app/user/fragments/detail_absen.dart';
+import 'package:project_attendance_app/user/fragments/detail_absen2.dart';
 import 'package:project_attendance_app/user/userPreferences/record_preferences.dart';
 import 'package:project_attendance_app/user/userPreferences/siswa_preference.dart';
 import 'package:project_attendance_app/user/userPreferences/user_preferences.dart';
@@ -64,6 +65,17 @@ class DrawerNavigation extends StatelessWidget {
               Navigator.pop(context);
               Future.delayed(const Duration(milliseconds: 500), () {
                 Get.to(() => const DetailAbsen());
+              });
+              // Add your logic here to navigate to Sent page
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.auto_graph_outlined),
+            title: Text('Detail Absensi 2'),
+            onTap: () {
+              Navigator.pop(context);
+              Future.delayed(const Duration(milliseconds: 500), () {
+                Get.to(() => TableEventsExample());
               });
               // Add your logic here to navigate to Sent page
             },
